956096ae2ca84599cfadbff3bdef6ef60bdd5b18
Karanvir Kang <Karankang007@gmail.com>
2015-02-16 20:52:32 -0500

    Initial checkin

---
+++ EchoClient/Program.cs
@@ -0,0 +1,21 @@
+﻿#region Header
+
+// Author: Karanvir Kang
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
